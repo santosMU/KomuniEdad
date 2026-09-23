7d51f29b74d5a8815c534bc23e6bbfcae676e036
Trent Joaqin I. Santos <tjisantos4@mymail.mapua.edu.ph>
2026-09-23 22:02:35 +0800

    fix: support Vercel Supabase environment names

--- config/komuniedad.php
+++ config/komuniedad.php
@@ -1,3 +1,10 @@
 <?php
 
-return ['demo' => env('KOMUNIEDAD_DEMO', false), 'url' => env('SUPABASE_URL'), 'key' => env('SUPABASE_ANON_KEY')];
+return [
+    'demo' => env('KOMUNIEDAD_DEMO', false),
+    'url' => env('SUPABASE_URL', env('NEXT_PUBLIC_SUPABASE_URL')),
+    'key' => env(
+        'SUPABASE_ANON_KEY',
+        env('SUPABASE_PUBLISHABLE_KEY', env('NEXT_PUBLIC_SUPABASE_ANON_KEY'))
+    ),
+];
